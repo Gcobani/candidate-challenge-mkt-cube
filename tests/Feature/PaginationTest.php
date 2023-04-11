a10324e722c871dae3e961e17ff11feaede66496
Gcobani Poswa <Gcobani@users.noreply.github.com>
2023-04-12 00:07:24 +0200

    update backward navigation test

--- tests/Feature/PaginationTest.php
+++ tests/Feature/PaginationTest.php
@@ -10,13 +10,14 @@ class PaginationTest extends TestCase
     public function test_pagination_can_navigate_froward_to_page_2(): void
     {
         $this->visit('/')->seeElement( 'a', [
-            'href' => 'http://localhost?page=2',
+            'href' => $this->baseUrl . '?page=2',
             'aria-label' => 'Next &raquo;'
         ]);
     }
 
     public function test_pagination_can_navigate_backwards_from_page_2_to_page_1()
     {
-        $this->visit('/?page=2')->seePageIs('/?page=2')->seeInElement('a', '1');
+        $this->visit('/?page=2')->seePageIs('/?page=2')
+            ->dontSeeElement('a', ['href' => $this->baseUrl . '?page=1']);
     }
 }
