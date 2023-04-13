f56831347af7379a91220d1d937b02676fbb2116
Gcobani Poswa <Gcobani@users.noreply.github.com>
2023-04-13 23:27:24 +0200

    Remove unknown query parameters from canonical tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-        <link rel="canonical" href="{{ request()->fullUrl() }}">
+        <link rel="canonical"
+              href="{{ request()->fullUrlWithQuery(array_fill_keys(array_keys(request()->except('page')), null)) }}">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
