773e42a7489aa3e1d4409a7812a374fac40077e4
Gcobani Poswa <Gcobani@users.noreply.github.com>
2023-04-14 03:21:22 +0200

    Fix add a canonical tag to the HTML head for all pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,8 +15,9 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-        <link rel="canonical"
-              href="{{ request()->fullUrlWithQuery(array_fill_keys(array_keys(request()->except('page')), null)) }}">
+        <link rel="canonical" @if(!empty(request()->except('page')))
+        href="{{ request()->fullUrlWithQuery(array_fill_keys(array_keys(request()->except('page')), null)) }}
+        @else href="{{request()->fullUrl()}}" @endif >
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
